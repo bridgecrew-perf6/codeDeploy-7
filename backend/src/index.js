53095525e73053086f53a6e8945fe4e56f9b0852
Lakshay Khurana <lakshay.khurana@block8.com>
2022-02-03 15:26:06 +0530

    hmm

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config();
 
 app.get("/hello", (req, res) => {
-  res.send(`Hello EARTH`);
+  res.send(`Hello EEARTH`);
 });
 
 module.exports.handler = serverless(app);
